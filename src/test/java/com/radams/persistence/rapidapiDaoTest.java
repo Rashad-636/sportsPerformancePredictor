63d94e1290a29d597dcbc2acc1e14fe70db7e421
Rashad Adams <Rashaddms@gmail.com>
2024-10-28 15:09:35 -0400

    Pom updates

--- src/test/java/com/radams/persistence/rapidapiDaoTest.java
+++ src/test/java/com/radams/persistence/rapidapiDaoTest.java
@@ -5,9 +5,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class rapidapiDaoTest {
+
     @Test
     void getTeamnameSucess() {
         RapidapiDao dao = new RapidapiDao();
         assertEquals("Magic", dao.getTeams().getBody().get(0).getTeamName());
     }
+
 }
